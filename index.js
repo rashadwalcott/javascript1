44b1005f02a7a1bd52d10e5c1cd9a34cdf9f0073
Rashad <rashad.walcott15@gmail.com>
2020-09-23 11:31:43 -0400

    array from range

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-// Create address object
-// street
-//city
-//zipCode
-//Create a function that shows the address objects and prints the key and values
 
-const address = {
-  street: "7520 Bell Blvd",
-  city: "Oakland Gardens",
-  zipCode: 11364,
-};
-
-function showAddress(address) {
-  for (key in address) console.log(key, address[key]);
-}
-
-showAddress(address);
