dbf5359713690ab91e70e5cc599f713d4736d585
Rashad <rashad.walcott15@gmail.com>
2020-09-23 15:26:03 -0400

    Getters and Setters

---
+++ functions/getAndSet.js
@@ -0,0 +1,21 @@
+const person = {
+  firstName: "Rashad",
+  lastName: "Walcott",
+  get fullName() {
+    return `${person.firstName} ${person.lastName}`;
+  },
+  set fullName(value) {
+    if (typeof value !== "string") throw new Error("Value is not a string");
+
+    const parts = value.split(" ");
+    if (parts.length !== 2) throw new Error("Enter a first and last name");
+    this.firstName = parts[0];
+    this.lastName = parts[1];
+  },
+};
+try {
+  person.fullName = "";
+} catch (e) {
+  alert(e);
+}
+console.log(person);
